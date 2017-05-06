568934fba18064f6590b019d7b3b65ae1aeae4f1
rohit sahay <rsahay414@gmail.com>
2017-05-06 20:01:03 +0530

    Jquery implementation
    jquery form validation added , jquery added throughout

--- INC App/WebContent/js/formValidation.js
+++ INC App/WebContent/js/formValidation.js
@@ -1,34 +1,40 @@
-function formValidateNew(){
-	var x = null;
-	var y = null;
-    var re = /^\w+$/;
-    
-    x=document.getElementById("cec").value;
-	y=document.getElementById("pass").value;
-    
-		  
-	console.log("the value of x= "+x);
-	console.log("the value of y= "+y);
+function prepareEventHandlers(){
+	document.getElementById("login_form").onsubmit = function (){
+		var x = null;
+		var y = null;
+	    var re = /^\w+$/;
+	    
+	    x=document.getElementById("cec").value;
+		y=document.getElementById("pass").value;
+	    
+			  
+		console.log("the value of x= "+x);
+		console.log("the value of y= "+y);
+	
+		if ( x == "" | y == "" ){
+		//validation for empty filed
+			   console.log("All Fields must be filled");
+			   window.alert("All Fields must be filled");
+			   return false;
+		}
+	    else if(x.length >=9 | y.length >=9 ){
+	       //validation for length of the fields
+	       console.log("CEC or Password cannot exceed 8 characters");
+	       window.alert("CEC or Password cannot exceed 8 characters");
+	       return false;
+	    }
+	    else if(!re.test(x)){
+	       //validation for special characters in filed
+	       window.alert("Username must contain only letters, numbers and underscores!");
+	       return false;
+	    }
+	    else{
+	       console.log("All validations are successfully met")
+	       return true;
+	    }
+	};
+}
 
-	if ( x == "" | y == "" ){
-	//validation for empty filed
-		   console.log("All Fields must be filled");
-		   window.alert("All Fields must be filled");
-		   return false;
-	}
-    else if(x.length >=9 | y.length >=9 ){
-       //validation for length of the fields
-       console.log("CEC or Password cannot exceed 8 characters");
-       window.alert("CEC or Password cannot exceed 8 characters");
-       return false;
-    }
-    else if(!re.test(x)){
-       //validation for special characters in filed
-       window.alert("Username must contain only letters, numbers and underscores!");
-       return false;
-    }
-    else{
-       console.log("All validations are successfully met")
-       return true;
-    }
+window.onload = function(){
+	prepareEventHandlers();
 }
--- INC App/WebContent/js/formValidation_jquery.js
+++ INC App/WebContent/js/formValidation_jquery.js
@@ -1,5 +1,33 @@
 $("document").ready(function(){
-	$("submit").click(function(){
-		alert("Text: " + $("#cec").val());
-	})
+	
+	$("#login_form").submit(function(){
+		var x = $('#cec').val();
+		var y = $('#pass').val();
+		var re = /^\w+$/;
+		
+		console.log("the value of x= "+x);
+		console.log("the value of y= "+y);
+		
+		if ( x == "" | y == "" ){
+			//validation for empty filed
+				   console.log("All Fields must be filled");
+				   alert("All Fields must be filled");
+				   return false;
+			}
+		    else if(x.length >=9 | y.length >=9 ){
+		       //validation for length of the fields
+		       console.log("CEC or Password cannot exceed 8 characters");
+		       alert("CEC or Password cannot exceed 8 characters");
+		       return false;
+		    }
+		    else if(!re.test(x)){
+		       //validation for special characters in filed
+		       alert("Username must contain only letters, numbers and underscores!");
+		       return false;
+		    }
+		    else{
+		       console.log("All validations are successfully met")
+		       return true;
+		    } 
+	});
 });
